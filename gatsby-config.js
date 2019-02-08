eaae18f492c2459a105cebf12e0737ee45ea6bf5
Bryan Besnyi <bryanbesnyi@Bryans-MacBook-Pro-2.local>
2019-02-08 00:20:42 -0800

    updated SiteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Accessibility Blog`,
     author: `Bryan Besnyi`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `Brief articles to help further your knowledge about accessibility.`,
+    siteUrl: `https://master.d2ssrym7i3949r.amplifyapp.com/`,
     social: {
       twitter: `BBesnyi`,
     },
